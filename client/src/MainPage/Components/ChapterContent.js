7f3d5f8a699f932c2df82d209b2ce363360cd35a
Hari-ansh <hariansh@attentive.ai>
2022-06-29 13:00:50 +0530

    chapter content dynamic

--- client/src/MainPage/Components/ChapterContent.js
+++ client/src/MainPage/Components/ChapterContent.js
@@ -1,44 +1,120 @@
-import { Menu , Button } from 'antd';
-import { ArrowRightOutlined,ArrowLeftOutlined } from '@ant-design/icons';
+import { Menu, Button } from "antd";
+import { ArrowRightOutlined, ArrowLeftOutlined } from "@ant-design/icons";
 
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+let ChapterDummyData = require("../../Data/ChapterDummyData.json");
 const ChapterContent = (props) => {
+  console.log("ChapterDummyData", ChapterDummyData);
+  const setChapterHeading =props.setChapterHeading
+  const chapterHeading= props.chapterHeading
   const collapsed = props.collapsed;
-  const setCollapsed= props.setCollapsed;
+  const setCollapsed = props.setCollapsed;
+  const setPdfURL = props.setPdfURL;
+  const pdfURL = props.pdfURL;
+  const videoURL = props.videoURL;
+  const setVideoURL = props.setVideoURL;
+
   const location = useLocation();
-  const [activeKey] = useState(location.pathname === '/stats' ? ['2'] : ['1']);
+  // const [activeKey] = useState(location.pathname === '/stats' ? ['2'] : ['1']);
 
-  function getItem(label, key, icon, children) {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    };
-  }
 
-  function itemOnClick({item, key, keyPath, domEvent}){
+
+  function itemOnClick({ item, key, keyPath, domEvent }) {
+    console.log("item", item);
+    console.log("key", key);
+
+    ChapterDummyData.forEach((chapter)=>{
+      if (key==chapter.chapter_number) {
+        setPdfURL(chapter.chapter_pdfURL)
+        setVideoURL(chapter.chapter_videoURL)
+        // setChapterHeading(chapter.chapter_name)
+        console.log('key',key);
+      
+    chapterItems.forEach((item)=>{
+      if(key ==item.key ){
+        setChapterHeading(item.label)
+        // console.log('chapterHeading',chapterHeading);
+      }
+    })
+    }
+    })
     
   }
 
-  const items = [
-    getItem("chapter1", "1"),
-    getItem("chapter2", "2"),
-    getItem("chapter3", "3"),
-    getItem("chapter4", "4"),
-    getItem("chapter5", "5"),
-  ];
+  var chapterItems = [];
 
-  return(
+  ChapterDummyData.forEach((chapter) => {
+    chapterItems.push({
+      key: chapter.chapter_number,
+      label: 'chapter'+chapter.chapter_number+': '+chapter.chapter_name,
+    });
+  });
+
+  console.log('chapterItems',chapterItems);
+
+ 
+
+  return (
     <div>
-      <div className ="top-spacing" style={{height: 'calc(8vh - 4px)', alignItems: 'center',  textAlign: 'center',display:'flex', justifyContent: 'space-around' }}>
-        <h1 style = {{color: '#212121',fontSize: 'medium', margin: 0, padding: '4px 0 0 0'}}>{collapsed ? '' : 'Chapter List'}</h1>
-        <Button style= {{background: '#4CBB7F'}} onClick={()=>{setCollapsed(!collapsed)}} type="primary" shape="circle" icon={collapsed ?<ArrowRightOutlined />:<ArrowLeftOutlined />} size={50} />
+      <div
+        className="top-spacing"
+        style={{
+          height: "calc(8vh - 4px)",
+          alignItems: "center",
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <h1
+          style={{
+            color: "#212121",
+            fontSize: "medium",
+            margin: 0,
+            padding: "4px 0 0 0",
+          }}
+        >
+          {collapsed ? "" : "Chapter List"}
+        </h1>
+        <Button
+          style={{ background: "#4CBB7F" }}
+          onClick={() => {
+            setCollapsed(!collapsed);
+          }}
+          type="primary"
+          shape="circle"
+          icon={collapsed ? <ArrowRightOutlined /> : <ArrowLeftOutlined />}
+          size={50}
+        />
       </div>
-      <Menu className = "chapter-sider" selectedKeys={activeKey} mode="inline" items={collapsed ? '' : items} onClick= {itemOnClick}/>
-    </div> 
+      {/* <Menu className = "chapter-sider" selectedKeys={activeKey} mode="inline" items={collapsed ? '' : items} onClick= {itemOnClick}/> */}
+      <Menu
+        className="chapter-sider"
+        mode="inline"
+        items={collapsed ? "" : chapterItems}
+        onClick={itemOnClick}
+      />
+    </div>
   );
-}
+};
 
 export default ChapterContent;
+
+  // function getItem(label, key, icon, children) {
+  //   return {
+  //     key,
+  //     icon,
+  //     children,
+  //     label,
+  //   };
+  // }
+   // const items = [
+  //   getItem("chapter1", "1"),
+  //   getItem("chapter2", "2"),
+  //   getItem("chapter3", "3"),
+  //   getItem("chapter4", "4"),
+  //   getItem("chapter5", "5"),
+  // ];
+  // console.log("items", items);
--- client/src/MainPage/Modules/Tutorial.js
+++ client/src/MainPage/Modules/Tutorial.js
@@ -1,20 +1,22 @@
 import { useParams } from "react-router-dom";
-import React, { useState } from 'react';
-import { Layout, Button, Checkbox, Row, Col, Breadcrumb} from 'antd';
+import React, { useState } from "react";
+import { Layout, Button, Checkbox, Row, Col, Breadcrumb } from "antd";
 import FooterContent from "../Components/FooterContent";
-import SiderContent from '../Components/SiderContent';
+import SiderContent from "../Components/SiderContent";
 import ChapterContent from "../Components/ChapterContent";
 
 const { Content, Sider, Footer } = Layout;
 
 const Tutorial = (props) => {
-  const {module} = useParams();
+  const { module } = useParams();
   const [collapsed, setCollapsed] = useState(true);
   const [collapsedSecond, setCollapsedSecond] = useState(true);
 
   const [showVideo, setShowVideo] = useState(true);
   const [showPdf, setShowPdf] = useState(true);
-
+  const [videoURL, setVideoURL] = useState("");
+  const [pdfURL, setPdfURL] = useState("");
+  const [chapterHeading, setChapterHeading] = useState("");
   const onVideoChange = (e) => {
     console.log(`checked = ${e.target.checked}`);
     setShowVideo(!showVideo);
@@ -26,52 +28,99 @@ const Tutorial = (props) => {
     setShowPdf(!showPdf);
   };
 
-  return(
-    <Layout style={{ minHeight: '100vh', maxHeight: '100vh'}} >
+  return (
+    <Layout style={{ minHeight: "100vh", maxHeight: "100vh" }}>
       <Sider collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <SiderContent collapsed = {collapsed}/>
+        <SiderContent collapsed={collapsed} />
       </Sider>
       <Layout>
-        <Content className = "site-layout-first">
-          <div style = {{padding: '2vw 2vw 0 2vw'}}>
+        <Content className="site-layout-first">
+          <div style={{ padding: "2vw 2vw 0 2vw" }}>
             <Breadcrumb>
-              <Breadcrumb.Item href = "/">My games</Breadcrumb.Item>
-              <Breadcrumb.Item href = "/">{module} Landscaping</Breadcrumb.Item>
-              <Breadcrumb.Item href = "/">Tutorial</Breadcrumb.Item>
+              <Breadcrumb.Item href="/">My games</Breadcrumb.Item>
+              <Breadcrumb.Item href="/">{module} Landscaping</Breadcrumb.Item>
+              <Breadcrumb.Item href="/">Tutorial</Breadcrumb.Item>
             </Breadcrumb>
             <br></br>
             <h1>Tutorials</h1>
           </div>
 
           <Layout>
-            <Sider className = "chapter-sider" trigger={null} collapsible collapsed={collapsedSecond} >
-              <ChapterContent setCollapsed={setCollapsedSecond} collapsed = {collapsedSecond}/>
+            <Sider
+              className="chapter-sider"
+              trigger={null}
+              collapsible
+              collapsed={collapsedSecond}
+            >
+              <ChapterContent
+              setChapterHeading={setChapterHeading}
+              chapterHeading={chapterHeading}
+                videoURL={videoURL}
+                setVideoURL={setVideoURL}
+                pdfURL={pdfURL}
+                setPdfURL={setPdfURL}
+                setCollapsed={setCollapsedSecond}
+                collapsed={collapsedSecond}
+              />
             </Sider>
             <Content>
               <div className="site-layout-second">
                 <Row>
-                  <Col span = {12}>
-                    <h1>Chapter 1: Introduction</h1>
+                  <Col span={12}>
+                    {/* <h1>Chapter 1: Introduction</h1> */}
+                    <h1>{chapterHeading}</h1>
                   </Col>
-                  <Col span = {2}>
-                    View
+                  <Col span={2}>View</Col>
+                  <Col span={4}>
+                    <Checkbox
+                      onChange={onVideoChange}
+                      defaultChecked={showVideo}
+                    >
+                      Video
+                    </Checkbox>
                   </Col>
-                  <Col span = {4}>
-                    <Checkbox onChange={onVideoChange} defaultChecked = {showVideo}>Video</Checkbox>
-                  </Col>
-                  <Col span = {4}>
-                    <Checkbox onChange={onPdfChange} defaultChecked = {showPdf}>Pdf</Checkbox>
+                  <Col span={4}>
+                    <Checkbox onChange={onPdfChange} defaultChecked={showPdf}>
+                      Pdf
+                    </Checkbox>
                   </Col>
                 </Row>
-                <Row style = {{minHeight: "65vh"}}>
-                  <Col span = {(showVideo && showPdf) ? 14 : 24} style = {{display: showVideo ? "block" : "none"}}>
-                    <div className="video-responsive" style = {{height: (showVideo && showPdf) ? "450px" : "500px", width: (showVideo && showPdf) ? "700px" : "850px"}}>
-                      <iframe src="https://drive.google.com/file/d/0B9XzsMuZIc4iLV9mYjV3a3JadTQ/preview" title = "Chapter Video" frameBorder={0}></iframe>
+                <Row style={{ minHeight: "65vh" }}>
+                  <Col
+                    span={showVideo && showPdf ? 14 : 24}
+                    style={{ display: showVideo ? "block" : "none" }}
+                  >
+                    <div
+                      className="video-responsive"
+                      style={{
+                        height: showVideo && showPdf ? "450px" : "500px",
+                        width: showVideo && showPdf ? "700px" : "850px",
+                      }}
+                    >
+                      <iframe
+                        src={videoURL}
+                        title="Chapter Video"
+                        frameBorder={0}
+                      ></iframe>
                     </div>
                   </Col>
-                  <Col span = {(showVideo && showPdf) ? 10 : 24} style = {{display: showPdf ? "block" : "none"}}>
-                    <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
-                      <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+                  <Col
+                    span={showVideo && showPdf ? 10 : 24}
+                    style={{ display: showPdf ? "block" : "none" }}
+                  >
+                    <object
+                      // data="http://africau.edu/images/default/sample.pdf"
+                      data={pdfURL}
+                      type="application/pdf"
+                      width="100%"
+                      height="100%"
+                    >
+                      <p>
+                        Alternative text - include a link{" "}
+                        <a href="http://africau.edu/images/default/sample.pdf">
+                          to the PDF!
+                        </a>
+                      </p>
                     </object>
                   </Col>
                 </Row>
@@ -79,12 +128,20 @@ const Tutorial = (props) => {
             </Content>
           </Layout>
         </Content>
-        <Footer style={{ textAlign: 'center' , height: '48px', alignItems : 'center', backgroundColor: '#fff', padding: 0 }}>
-          <FooterContent/>
+        <Footer
+          style={{
+            textAlign: "center",
+            height: "48px",
+            alignItems: "center",
+            backgroundColor: "#fff",
+            padding: 0,
+          }}
+        >
+          <FooterContent />
         </Footer>
       </Layout>
     </Layout>
   );
-}
+};
 
 export default Tutorial;
