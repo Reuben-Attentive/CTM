ce4593c6c31e7da450066c97db660cd3c7c5118b
Hari-ansh <hariansh@attentive.ai>
2022-07-04 19:47:24 +0530

    performance page UI- tutorial-radio buttons

--- client/src/MainPage/Components/ChapterSidebar.js
+++ client/src/MainPage/Components/ChapterSidebar.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
-import { Menu, Button, Layout, Checkbox, Row, Col, Progress } from "antd";
+import {
+  Menu,
+  Button,
+  Layout,
+  Checkbox,
+  Radio,
+  Row,
+  Col,
+  Progress,
+  RadioChangeEvent,
+} from "antd";
 import { ArrowRightOutlined, ArrowLeftOutlined } from "@ant-design/icons";
 const { Content, Sider } = Layout;
 
@@ -8,9 +18,14 @@ const ChapterSidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [showVideo, setShowVideo] = useState(true);
   const [showPdf, setShowPdf] = useState(false);
-  const [videoURL, setVideoURL] = useState(ChapterDummyData[0].chapter_videoURL);
+  const [videoURL, setVideoURL] = useState(
+    ChapterDummyData[0].chapter_videoURL
+  );
   const [pdfURL, setPdfURL] = useState(ChapterDummyData[0].chapter_pdfURL);
-  const [chapterHeading, setChapterHeading] = useState("Chapter 1 : "+ChapterDummyData[0].chapter_name);
+  const [chapterHeading, setChapterHeading] = useState(
+    "Chapter 1 : " + ChapterDummyData[0].chapter_name
+  );
+  const [view, setView] = useState("Video");
 
   const onVideoChange = (e) => {
     setShowVideo(!showVideo);
@@ -106,17 +121,20 @@ const ChapterSidebar = () => {
       </Sider>
       <Content>
         <div className="site-layout-second">
-          <Row style={{marginBottom : 10 }}>
-            <Col span={12}>
+          <Row style={{ marginBottom: 10 }}>
+            <Col span={18}>
               <h1
-              style={{
-                color: "#212121",
-                fontSize: "medium",
-                margin: 0,}}>
-              {chapterHeading}</h1>
+                style={{
+                  color: "#212121",
+                  fontSize: "medium",
+                  margin: 0,
+                }}
+              >
+                {chapterHeading}
+              </h1>
             </Col>
-            <Col span={2}>View</Col>
-            <Col span={4}>
+            <Col span={1}>View</Col>
+            {/* <Col span={4}>
               <Checkbox onChange={onVideoChange} defaultChecked={showVideo}>
                 Video
               </Checkbox>
@@ -125,18 +143,22 @@ const ChapterSidebar = () => {
               <Checkbox onChange={onPdfChange} defaultChecked={showPdf}>
                 Pdf
               </Checkbox>
+            </Col> */}
+            <Col>
+              <Radio.Group onChange={(e: RadioChangeEvent)=>{setView(e.target.value);}} value={view}>
+                <Radio value={"Video"}>Video</Radio>
+                <Radio value={"Pdf"}>Pdf</Radio>
+                <Radio value={"Both"}>Both</Radio>
+              </Radio.Group>
             </Col>
           </Row>
           <Row style={{ minHeight: "65vh" }}>
-            <Col
-              span={showVideo && showPdf ? 14 : 24}
-              style={{ display: showVideo ? "block" : "none" }}
-            >
-              <div
+          <Col span= {view=="Video"?24: view=="Both" ?14:0} >
+          <div
                 className="video-responsive"
                 style={{
-                  height: showVideo && showPdf ? "450px" : "500px",
-                  width: showVideo && showPdf ? "660px" : "850px",
+                  height: view=="Both" ? "450px" : "500px",
+                  width: view=="Both" ? "642px" : "850px",
                 }}
               >
                 <iframe
@@ -145,24 +167,21 @@ const ChapterSidebar = () => {
                   frameBorder={0}
                 ></iframe>
               </div>
-            </Col>
-            <Col
-              span={showVideo && showPdf ? 10 : 24}
-              style={{ display: showPdf ? "block" : "none" }}
-            >
-              <object
+          </Col>
+          <Col span= {view=="Pdf"?24: view=="Both" ?10:0} style={{   paddingLeft: 10}}>
+          <iframe src={pdfURL} width="100%" height="100%" allow="autoplay"></iframe>
+          {/* <object
                 data={pdfURL}
                 type="application/pdf"
                 width="100%"
                 height="100%"
               >
                 <p>
-                  <a href={pdfURL}>
-                    Link to the PDF!
-                  </a>
+                  <a href={pdfURL}>Link to the PDF!</a>
                 </p>
-              </object>
-            </Col>
+              </object> */}
+          </Col>
+            
           </Row>
         </div>
       </Content>
--- client/src/MainPage/Modules/Performance.js
+++ client/src/MainPage/Modules/Performance.js
@@ -1,19 +1,31 @@
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Layout, Row, Col, Breadcrumb } from "antd";
+import { Layout, Row, Col, Breadcrumb, Select, Button } from "antd";
 import FooterBar from "../Components/FooterBar";
 import SideNavbar from "../Components/SideNavbar";
 
 const { Content } = Layout;
+const { Option } = Select;
 
 const Performance = () => {
   const { module } = useParams();
+  const performance_dummy_data = require("../../Data/PerformanceData.json");
+  const dummy_data =
+    performance_dummy_data.commercial_landscaping_practice.level_1.trial_1
+      .property_2.lens_url;
+  const [lensURL, setLensURL] = useState("");
+  // setLensURL(performance_dummy_data.commercial_landscaping_practice.level_1.trial_1.property_1.lens_url);
+  console.log("lensURL", dummy_data);
 
+  function SelectTrial(value) {
+    console.log(`selected ${value}`);
+  }
   return (
     <Layout style={{ minHeight: "100vh", maxHeight: "100vh" }}>
-      <SideNavbar/>
+      <SideNavbar />
       <Layout>
         <Content className="site-layout-first">
-          <div style={{ padding: "2vw 2vw 0 2vw" }}>
+          <div style={{ padding: "2vw 2vw 0 1vw" }}>
             <Breadcrumb>
               <Breadcrumb.Item href="/">My games</Breadcrumb.Item>
               <Breadcrumb.Item href="/">{module}</Breadcrumb.Item>
@@ -21,15 +33,149 @@ const Performance = () => {
               <Breadcrumb.Item href="/">Performance</Breadcrumb.Item>
             </Breadcrumb>
             <br></br>
-            <h1>Performance ({module})</h1>
-          </div>   
+            <h1 className="performance-h1">Performance ({module})</h1>
+          </div>
           <div>
-            <Row style = {{height: "78vh"}}>
-              <Col span = {6} style = {{backgroundColor: "#F5F5F5"}}>
+            <Row style={{ height: "58vh" }}>
+              <Col span={6} style={{ padding: 10, backgroundColor: "#F5F5F5" }}>
+                <div className="performance-h2">Level-1 </div>
+                <div className="flex-space-between-center">
+                  <p style={{ margin: 0 }}>Select trial</p>
+                  <Select
+                    defaultValue="trial_1"
+                    style={{ width: 100 }}
+                    onChange={SelectTrial}
+                  >
+                    <Option value="trial_1">Trial 1</Option>
+                    <Option value="trial_2">Trial 2</Option>
+                    <Option value="trial_3">Trial 3</Option>
+                    <Option value="trial_4">Trial 4</Option>
+                  </Select>
+                </div>
+                <div
+                  style={{
+                    background: "#DFF4E9",
+                    width: 227,
+                    height: 93,
+                    borderRadius: 13,
+                    margin: "10px 0px 10px 0px",
+                    padding: "10px",
+                  }}
+                >
+                  
+                  Trial 1 Score
+                </div>
+                <hr style={{ border: "1px solid", opacity: 0.26 }} />
+                {/* <p className="performance-h2"> Performance details</p> */}
+                <div className="performance-h2"> Performance details</div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  <p className="no-margin-padding">Select property</p>
+                  <div>
+                    <div
+                      className="flex-space-between-center"
+                      style={{
+                        marginBottom: 5,
+                      }}
+                    >
+                      <Button type="link" size="small">
+                        Previous
+                      </Button>
+                      <Button type="link" size="small">
+                        Next
+                      </Button>
+                    </div>
+                    <Select
+                      defaultValue="property_1"
+                      style={{ width: 120 }}
+                      onChange={SelectTrial}
+                    >
+                      <Option value="property_1">Property 1</Option>
+                      <Option value="property_2">Property 2</Option>
+                      <Option value="property_3">Property 3</Option>
+                      <Option value="property_4">Property 4</Option>
+                    </Select>
+                  </div>
+                </div>
+                <p
+                  className="performance-h2"
+                  style={{ margin: "20px 0px 10px 0px" }}
+                >
+                  Property 1
+                </p>
+                <div>
+                  <div className="performance-parameters">
+                    <div>Property score</div>
+                    {/* <p>Property score</p> */}
 
+                    <div
+                      style={{
+                        height: "25px",
+                        background: "#FFA500",
+                        padding: 3,
+                      }}
+                    >
+                      8/10
+                    </div>
+                  </div>
+                  <div className="performance-parameters">
+                    <div>Accuracy</div>
+                    {/* <p>Accuracy</p> */}
+                    <div
+                      style={{
+                        height: "25px",
+                        background: "#3BB44A",
+                        padding: 3,
+                      }}
+                    >
+                      Good
+                    </div>
+                  </div>
+                  <div className="performance-parameters">
+                    <div>ML usage</div>
+                    <div>ok</div>
+                  </div>
+                  <div className="performance-parameters">
+                    <div>Time spent</div>
+                    <div>bad</div>
+                  </div>
+                  <div className="performance-parameters">
+                    Time taken: 35 min
+                  </div>
+                  <div className="performance-parameters">
+                    Ideal time range: 5 to 10 min
+                  </div>
+
+                  <div style={{ marginTop: 50 }}>
+                    <div className="flex-center">
+                      <div className="performance-red-dot" />
+                      <div>Score 1 to 3 (Bad)</div>
+                    </div>
+                    <div className="flex-center">
+                      <div className="performance-orange-dot" />
+                      <div>Score 1 to 3 (Ok)</div>
+                    </div>
+                    <div className="flex-center">
+                      <div className="performance-green-dot" />
+                      <div>Score 1 to 3 (Good)</div>
+                    </div>
+                  </div>
+                </div>
               </Col>
-              <Col span = {18} style = {{padding: "10px"}}>
-                <iframe src="https://lens.attentive.ai/shared-view?id=62b197dddb71a016e4aa92da" title="Lens Link" frameBorder={0} allowfullscreen sandbox>
+              <Col span={18} style={{ padding: "10px" }}>
+                {/* <iframe src="https://lens.attentive.ai/shared-view?id=62b197dddb71a016e4aa92da" title="Lens Link" frameBorder={0} allowfullscreen sandbox> */}
+                <iframe
+                  src={dummy_data}
+                  title="Lens Link"
+                  frameBorder={0}
+                  allowfullscreen
+                  sandbox
+                >
                   <p>
                     <a href="https://lens.attentive.ai/shared-view?id=62b197dddb71a016e4aa92da">
                       Fallback link for browsers that don't support iframes
@@ -38,9 +184,9 @@ const Performance = () => {
                 </iframe>
               </Col>
             </Row>
-          </div>       
+          </div>
         </Content>
-        <FooterBar/>
+        <FooterBar />
       </Layout>
     </Layout>
   );
